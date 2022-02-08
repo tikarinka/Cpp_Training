a3ccf6f3074916d68bf2d8b8aa5fea62fdcf65c4
tikarinka <tikarinka@gmail.com>
2022-02-08 21:29:13 +0300

    Add Task 2 to Homework repo

---
+++ Homework/Task2/Task2/Task2.cpp
@@ -0,0 +1,59 @@
+﻿
+#include <algorithm>
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+	vector<string> Vector = {
+		"Иванов",
+		"Петров",
+		"Сидоров",
+		"Кошкин",
+		"Собакин",
+		"Медведев",
+		"Степашкин",
+		"Синицин",
+		"Пчёлкин",
+		"Перепёлкин",
+	};
+
+	cout << "Элементы вектора:" << endl;
+
+	for_each(Vector.begin(), Vector.end(), [](string data)
+	{
+		cout << data << endl;
+	});
+
+	cout << endl << "Отсортированные элементы вектора:" << endl;
+
+	unsigned int permutationsNumber = 0;
+
+	sort(Vector.begin(), Vector.end(), [&permutationsNumber](string data1, string data2)->bool
+	{
+		if (data1 < data2)
+		{
+			permutationsNumber++;
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	});
+
+	for_each(Vector.begin(), Vector.end(), [](string data)
+	{
+		cout << data << endl;
+	});
+
+	cout << endl << "Количество перестановок: " << permutationsNumber << endl;
+
+	return 0;
+}
+
